ecf465ccd7d33db2685954596ab463b90cf78c48
yungshenglu <yungshenglu1994@gmail.com>
2018-06-14 08:48:13 +0800

    LC-300 accepted

--- LC-300/LC-300.cpp
+++ LC-300/LC-300.cpp
@@ -6,26 +6,30 @@
 
 using namespace std;
 
-class Solution {
-public:
-    int lengthOfLIS(vector<int>& nums) {
-        // If no number in list, then break
-        if (nums.size() == 0)
-            return 0;
-        
+int main(void) {
+    int N;
+
+    while (scanf("%d", &N) != EOF && N != 0) {
+        int list[MAX] = { 0 };
+        for (int i = 0; i < N; ++i)
+            scanf("%d", &list[i]);
+
         // Use DP to find LIS
         int LIS[MAX] = { 1 }, length = 1;
-        for (int i = 0; i < nums.size(); ++i) {
+        for (int i = 0; i < N; ++i) {
             int longest = 0;
             for (int j = 0; j < i; ++j) {
-                if (nums[j] < nums[i])
+                if (list[j] < list[i])
                     longest = max(longest, LIS[j]);
             }
             
             LIS[i] = longest + 1;
             length = max(length, LIS[i]);
         }
-        
-        return length;
+
+        // Print the result
+        printf("%d\n", length);
     }
-};
+    
+    return 0;
+}
