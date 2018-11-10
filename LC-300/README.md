dc36d1c0455e2139452ab8aae209169df3138613
Yung-Sheng Lu <yungshenglu1994@gmail.com>
2018-11-10 15:42:46 +0800

    LC-566 accepted

--- LC-300/README.md
+++ LC-300/README.md
@@ -2,21 +2,21 @@
 
 Given an unsorted array of integers, find the length of longest increasing subsequence (LIS).
 
-> * Difficulty: MEDIUM
-> * Total Accepted: 132K
-> * Total Submissions: 338.4K
+> * Difficulty: **MEDIUM**
 
 ---
-## Example
+## Examples
 
 ```
 Input: [10, 9, 2, 5, 3, 7, 101, 18]
+
 Output: 4
+
 Explanation: The longest increasing subsequence is [2, 3, 7, 101], therefore the length is 4.
 ```
 
 ---
-## Note
+## Notes
 
 * There may be more than one LIS combination, it is only necessary for you to return the length.
 * Your algorithm should run in $O(n^2)$ complexity.
---
+++ LC-566/LC-566.cpp
@@ -0,0 +1,36 @@
+#include <cstdio>
+#include <vector>
+#include <queue>
+
+class Solution {
+public:
+    /*
+     * Concepts:
+     * 1. Create a queue to store all elements in the matrix
+     * 2. Reshape the matrix with element in the queue
+     */
+    vector< vector<int> > matrixReshape(vector< vector<int> >& nums, int r, int c) {
+        int orig_r = nums.size(), orig_c = nums[0].size(), orig_size = orig_r * orig_c;
+        
+        if (r * c != orig_size)
+            return nums;
+        else {
+            queue<int> que;
+            for (int i = 0; i < orig_r; ++i) {
+                for (int j = 0; j < orig_c; ++j)
+                    que.push(nums[i][j]);
+            }
+            
+            vector< vector<int> > reshape(r);
+            for (int i = 0; i < r; ++i) {
+                reshape[i] = vector<int>(c);
+                for (int j = 0; j < c; ++j) {
+                    reshape[i][j] = que.front();
+                    que.pop();
+                }
+            }
+            
+            return reshape;
+        }
+    }
+};
