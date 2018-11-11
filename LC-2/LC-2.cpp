c18568c0bd75153dd1c50916d53dd88100cd7b72
Yung-Sheng Lu <yungshenglu1994@gmail.com>
2018-11-11 13:40:04 +0800

    LC-2 Accepted

---
+++ LC-2/LC-2.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    /**
+     * Concepts:
+     * Append the result of the sum of each digit at the end of list and notice the carry
+     */
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode *curr1 = l1, *curr2 = l2;
+        ListNode *result = new ListNode(NULL), *head = result;
+        int carry = 0;
+
+        while (curr1 != NULL || curr2 != NULL) {
+            int sum = 0;
+            if (curr1 == NULL && curr2 != NULL)
+                sum = curr2->val + carry;
+            else if (curr2 == NULL && curr1 != NULL)
+                sum = curr1->val + carry;
+            else
+                sum = curr1->val + curr2->val + carry;
+            
+            carry = sum / 10;
+            ListNode *node = new ListNode(sum % 10);
+            
+            head->next = node;
+            head = head->next;
+            curr1 = (curr1 == NULL) ? NULL : curr1->next;
+            curr2 = (curr2 == NULL) ? NULL : curr2->next;  
+        }
+        
+        // Append the last carry at the end of list
+        if (carry != 0) {
+            ListNode *node = new ListNode(carry);
+            head->next = node;
+        }
+        
+        return result->next;
+    }
+};
---
+++ LC-2/README.md
@@ -0,0 +1,35 @@
+# LC-2 - Add Two Numbers
+
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+> * Difficulty: **MEDIUM**
+
+---
+## Examples
+
+```
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+Explanation: 342 + 465 = 807.
+```
+
+```
+Input: (5) + (5)
+Output: 0 -> 1
+Explanation: 5 + 5 = 10.
+```
+
+```
+Input: (1 -> 8) + (0)
+Output: 1 -> 8
+Explanation: 18 + 0 = 18.
+```
+
+---
+## Solutions
+
+1. Elementary Math
+    * Append the result of the sum of each digit at the end of list
+    * Notice the carry after the result of sum
