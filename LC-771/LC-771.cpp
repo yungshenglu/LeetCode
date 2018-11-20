f5485f4daa2821c4a008b386dc946aba498a495c
Yung-Sheng Lu <yungshenglu1994@gmail.com>
2018-11-20 19:25:59 +0800

    LC-771 accepted

---
+++ LC-771/LC-771.cpp
@@ -0,0 +1,21 @@
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    /**
+     * Concepts:
+     * Naive Approach
+     */
+    int numJewelsInStones(string J, string S) {
+        int count = 0;
+        for (int i = 0; i < S.length(); ++i) {
+            for (int j = 0; j< J.length(); ++j) {
+                if (S[i] == J[j])
+                    ++count;
+            }
+        }
+        return count;
+    }
+};
