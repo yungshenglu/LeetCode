4ca42dcee1988c31b0add7fa7da6078b6a53ffaf
Yung-Sheng Lu <yungshenglu1994@gmail.com>
2018-11-23 17:17:51 +0800

    LC-191 accepted

--- LC-191/LC-191-2.cpp
+++ LC-191/LC-191-2.cpp
@@ -6,7 +6,8 @@ using namespace std;
 class Solution {
     /**
      * Concepts: Bit Manipulation Trick
-     * 
+     * 1. Repeatedly flip the least-significant 1-bit of the number to 0, and add 1 to the sum 
+     * 2. As soon as the number becomes 0, we know that it does not have any more 1-bits, and we return the sum
      */
 public:
     int hammingWeight(uint32_t n) {
