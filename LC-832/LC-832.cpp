ff0aa661241bce52ca32aac99d5f4d041746fd32
Yung-Sheng Lu <yungshenglu1994@gmail.com>
2018-11-27 12:02:02 +0800

    LC-832 accepted

---
+++ LC-832/LC-832.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+    /**
+     * Concepts: Naive Approach
+     * 1. Use floor division to iterate each row in the image
+     * 2. Use XOR 1 to flip each pixel in the row
+     */
+public:
+    vector< vector<int> > flipAndInvertImage(vector< vector<int> >& A) {
+        int c = A[0].size();
+        for (int i = 0; i < A.size(); ++i) {
+            for (int j = 0; j < (c + 1) / 2; ++j) {
+                int tmp = A[i][j] ^ 1;
+                A[i][j] = A[i][c - 1 - j] ^ 1;
+                A[i][c - 1 - j] = tmp;
+            }
+        }
+        return A;
+    }
+};
