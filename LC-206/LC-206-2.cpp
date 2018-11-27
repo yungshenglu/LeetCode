226dcf3755c1f03ca2a2e8b984450810f4cae88f
Yung-Sheng Lu <yungshenglu1994@gmail.com>
2018-11-27 12:17:43 +0800

    LC-206 accepted

--- LC-206/LC-206-2.cpp
+++ LC-206/LC-206-2.cpp
@@ -2,12 +2,12 @@ using namespace std;
 
 /**
  * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
  */
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
 
 class Solution {
 public:
