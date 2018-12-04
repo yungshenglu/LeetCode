59beae5f1225f9aa78dfeca1d0c744d03e615c56
Yung-Sheng Lu <yungshenglu1994@gmail.com>
2018-12-04 12:51:59 +0800

    LC-1 accepted

---
+++ LC-1/LC-1.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ */ 
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    /**
+     * Concepts: Brute Force
+     * Find out each pair of element in the list that sum is equal to target
+     */
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            for (int j = i + 1; j < nums.size(); ++j) {
+                if (nums[i] == target - nums[j]) {
+                    return vector<int>{i, j};
+                }
+            }
+        }
+        return result;
+    }
+};
---
+++ LC-1/README.md
@@ -0,0 +1,25 @@
+# LC-1 - Two Sum
+
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+> * Difficulty: **MEDIUM**
+
+---
+## Examples
+
+```
+Input: nums = [2, 7, 11, 15], target = 9
+Output: [0, 1]
+Explanation: Because nums[0] + nums[1] = 2 + 7 = 9, return [0, 1].
+```
+
+---
+## Solutions
+
+1. Brute Force
+    * Time complexity: $O(n^2)$
+    * Space complexity: $O(1)$
