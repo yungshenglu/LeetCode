478779e917d56fdab9174100b1a6d3967d0df151
Yung-Sheng Lu <yungshenglu1994@gmail.com>
2018-12-04 12:50:05 +0800

    LC-2 accepyed

--- LC-2/LC-2.cpp
+++ LC-2/LC-2.cpp
@@ -1,11 +1,14 @@
+using namespace std;
+
 /**
  * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+ */ 
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
 class Solution {
 public:
     /**
