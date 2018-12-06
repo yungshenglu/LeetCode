1f095c41226ade2d8146de3e35e3e012843e1694
Yung-Sheng Lu <yungshenglu1994@gmail.com>
2018-12-06 11:45:17 +0800

    LC-442

---
+++ LC-442/LC-442-1.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    /**
+     * Concepts: Hash Table
+     * 1. Create a hash table to store the frequency of each number in the list
+     * 2. Count the frequency of each number in the list and check whether the frequency is greater than 1 in each iteration
+     */
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> hash(nums.size(), 0);
+        vector<int> result;
+        for (int i = 0; i < nums.size(); ++i) {
+            ++hash[nums[i] - 1];
+            if (hash[nums[i] - 1] > 1)
+                result.push_back(nums[i]);
+        }
+        return result;
+    }
+};
---
+++ LC-442/README.md
@@ -0,0 +1,27 @@
+# LC-442 - Find All Duplicates in an Array
+
+Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+
+Find all the elements that appear twice in this array.
+
+> * Difficulty: **MEDIUM**
+
+---
+## Examples
+
+```
+Input: [4,3,2,7,8,2,3,1]
+Output: [2,3]
+```
+
+---
+## Follow Up
+
+* Could you do it without extra space and in O(n) runtime?
+
+---
+## Solutions
+
+1. Hash Table
+    * Time complexity: $O(n)$
+    * Space complexity: $O(n)$
