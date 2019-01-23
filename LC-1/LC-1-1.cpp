4744057631806cc28ed49b85135af9393bb57192
Yung-Sheng Lu <yungshenglu1994@gmail.com>
2019-01-23 16:39:33 +0800

    LC-1 accepted

--- LC-1/LC-1-1.cpp
+++ LC-1/LC-1-1.cpp
@@ -2,15 +2,6 @@
 
 using namespace std;
 
-/**
- * Definition for singly-linked list.
- */ 
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
-
 class Solution {
 public:
     /**
--- LC-1/LC-1-3.cpp
+++ LC-1/LC-1-3.cpp
@@ -3,15 +3,6 @@
 
 using namespace std;
 
-/**
- * Definition for singly-linked list.
- */ 
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
-
 class Solution {
 public:
     /**
