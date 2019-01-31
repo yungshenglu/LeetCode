66d99a80db81f5c29528d15df6c0128323f5866e
Yung-Sheng Lu <yungshenglu1994@gmail.com>
2019-01-31 15:50:56 +0800

    LC-908 accepted

---
+++ LC-908/LC-908.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    /**
+     * Concepts: Math Approach
+     * 1. Find out the maximum and minimum element in the array
+     * 2. The smallest difference is:
+     *    max - min - 2 * K
+     * 3. Make sure the smallest difference is positive
+     */
+    int smallestRangeI(vector<int>& A, int K) {
+        int min = A[0], max = A[0];
+        for (int i = 1; i < A.size(); ++i) {
+            max = (A[i] > max) ? A[i] : max;
+            min = (A[i] < min) ? A[i] : min;
+        }
+        int result = max - min - 2 * K;
+        return (result > 0) ? result : 0;
+    }
+};
