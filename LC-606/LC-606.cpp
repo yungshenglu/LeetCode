6a6516bb52b7840880a1b788616995b1e8dcd239
Yung-Sheng Lu <yungshenglu1994@gmail.com>
2019-03-16 14:23:30 +0800

    LC-606 accepted

---
+++ LC-606/LC-606.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <string>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node
+ */ 
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    /**
+     * Concepts: Recursion
+     */
+    string tree2str(TreeNode* t) {
+        if (t == NULL)
+            return "";
+        else if (t->left == NULL && t->right == NULL)
+            return to_string(t->val);
+        else if (t->right == NULL)
+            return to_string(t->val) + "(" + tree2str(t->left) + ")";
+        return to_string(t->val) + "(" + tree2str(t->left) + ")(" + tree2str(t->right) + ")";
+    }
+};
