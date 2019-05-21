597e8b17148a80bac42fe7d7493d374de3887e1b
yungshenglu <yungshenglu1994@gmail.com>
2019-05-21 13:31:31 +0800

    LC-429 accepted

---
+++ LC-429/LC-429.cpp
@@ -0,0 +1,44 @@
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ */
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+    Node() {}
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+class Solution {
+public:
+    /**
+     * Concepts: Breadth-First Search (BFS)
+     */
+    vector<vector<int>> levelOrder(Node* root) {
+        vector< vector<int> > result;
+        queue<Node*> que;
+        que.push(root);
+        
+        while (!que.empty() && root) {
+            vector<int> child;
+            int qSize = que.size();
+            for (int i = 0; i < qSize; ++i) {
+                Node* node = que.front();
+                que.pop();
+                child.push_back(node->val);
+                for (int j = 0; j < node->children.size(); ++j)
+                    que.push(node->children[j]);
+            }
+            result.push_back(child);
+        }
+        return result;
+    }
+};
---
+++ LC-429/README.md
@@ -0,0 +1,38 @@
+# LC-429 - Trim a Binary Search Tree
+
+Given an n-ary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+For example, given a `3-ary` tree:
+
+![](../res/img/LC-429.png)
+
+> * Difficulty: **EASY**
+
+---
+## Examples
+
+```
+Input:
+{"$id":"1","children":[{"$id":"2","children":[{"$id":"5","children":[],"val":5},{"$id":"6","children":[],"val":6}],"val":3},{"$id":"3","children":[],"val":2},{"$id":"4","children":[],"val":4}],"val":1}
+Output:
+[
+     [1],
+     [3,2,4],
+     [5,6]
+]
+```
+
+![](../res/img/LC-429.png)
+
+---
+## Notes
+
+* The depth of the tree is at most `1000`.
+* The total number of nodes is at most `5000`.
+
+---
+## Solutions
+
+1. Breadth-First Search (BFS)
+    * Time complexity: $O(T)$
+    * Space complexity: $O(n^2)$
