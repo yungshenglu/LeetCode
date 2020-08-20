36f870923b6ed13ecbb464455dcca457ac5dff54
davidlu <yungshenglu1994@gmail.com>
2020-08-20 12:07:25 +0800

    LC-746 TypeScript accepted

--- LC-746/LC-746.ts
+++ LC-746/LC-746.ts
@@ -1,3 +1,10 @@
+/**
+ * Concepts: Dynamic Programming
+ * 1. Evaluate backwards in order to decide cost[i] after figuring out cost[i + 1] and cost[i + 2]
+ * 2. Let f1 and f2 be the old value of cost[i + 1] and cost[i + 2]
+ * 3. Iterate through i backwards and return min(f1, f2)
+ */
+
 function minCostClimbingStairs(cost: number[]): number {
   let f1: number = 0,
     f2: number = 0
